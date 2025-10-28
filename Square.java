6f4fc40f28bfa1ac4f3f3675da8888091c4f01c7
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-10-28 07:56:38 +0700

    Tugas2

---
+++ Square.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int N = sc.nextInt();
+
+        // Outer loop = jumlah baris
+        for (int outer = 1; outer <= N; outer++) {
+            // Inner loop = jumlah kolom
+            for (int inner = 1; inner <= N; inner++) {
+                System.out.print("*");
+            }
+            System.out.println(); // pindah baris setelah 1 baris selesai
+        }
+
+        sc.close();
+    }
+}
