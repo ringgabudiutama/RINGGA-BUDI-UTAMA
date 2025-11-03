610fec1d2776f20fade6b9ee607f5cc80b61aa87
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-11-03 22:30:02 +0700

    push

--- JOBSHEET7/TheTriangleNoAbsen.java
+++ JOBSHEET7/TheTriangleNoAbsen.java
@@ -2,33 +2,16 @@ import java.util.Scanner;
 
 public class TheTriangleNoAbsen {
     public static void main(String[] args) {
-     
         Scanner input = new Scanner(System.in);
-
-   
         int numInput;
-
-        int i = 0;
-
         String s = "";
 
         System.out.print("Input some number: ");
         numInput = input.nextInt();
-    
-        input.close();
 
-        System.out.println("Input some number: " + numInput);
-
-       
-        while (i < numInput) {
-        
+        for (int i = 0; i < numInput; i++) {
             s += "*";
-            
-          
             System.out.println(s);
-            
-          
-            i++;
         }
     }
 }
