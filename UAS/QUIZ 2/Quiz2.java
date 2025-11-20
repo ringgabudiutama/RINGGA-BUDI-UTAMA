186a5ad92f45ca69eaa55e012725dfac01ce09d8
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-11-20 14:37:03 +0700

    Uas

--- UAS/QUIZ 2/Quiz2.java
+++ UAS/QUIZ 2/Quiz2.java
@@ -1,8 +1,6 @@
-package UAS.UAS;
-
 import java.util.Scanner;
 // Mulai dari sini ( start )
-public class UasRingga {
+public class Quiz2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
