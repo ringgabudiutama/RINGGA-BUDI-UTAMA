60bea8a445b3b6e481c71b6459c0d8ec9e6c22e8
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-12-02 09:28:52 +0700

    push

---
+++ JOBSHEET11/SummationRecursive.java
@@ -0,0 +1,20 @@
+package JOBSHEET11;
+
+import java.util.Scanner;
+
+public class SummationRecursive {
+
+    static int summation(int n){
+        if(n == 0)
+            return 0;
+        return n + summation(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int n = input.nextInt();
+
+        System.out.println("Summation result = " + summation(n));
+    }
+}
