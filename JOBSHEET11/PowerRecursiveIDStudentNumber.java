301ca69091fbe0c3e88ee90619ac9a05344aee85
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-12-02 09:21:20 +0700

    push

---
+++ JOBSHEET11/PowerRecursiveIDStudentNumber.java
@@ -0,0 +1,27 @@
+package JOBSHEET11;
+
+import java.util.Scanner;
+
+public class PowerRecursiveIDStudentNumber {
+
+   
+    static int calculatePower(int base, int pow) {
+        if (pow == 0)
+            return 1;
+        else
+            return base * calculatePower(base, pow - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input Base Number: ");
+        int base = input.nextInt();
+
+        System.out.print("Input Power Number: ");
+        int power = input.nextInt();
+
+        System.out.println("Result of " + base + "^" + power + " = " +
+                calculatePower(base, power));
+    }
+}
